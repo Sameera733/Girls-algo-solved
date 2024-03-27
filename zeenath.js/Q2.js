6eb7cdbf14b5ee4eeaaabe717819212e1400d3e5
zeenathkousar11 <zeenath7396@gmail.com>
2024-03-27 11:15:12 +0530

    zeenath's commit

--- zeenath.js/Q2.js
+++ zeenath.js/Q2.js
@@ -1,43 +1,37 @@
-
-function DisplayingEvenNumbersLessThanN(n)
+//even num less than n
+function genEvennumberslessthann(n){
+    let evennumbers=[]
+    for(i=0;i<10;i+=2){
+evennumbers.push(i)
+}
+return evennumbers;
+}
+console.log("generate even numbers less than n", ":",genEvennumberslessthann(10))
+//odd numbers less than n
+function oddnumberslessthann(n)
 {
-    console.log("the even numbers less than 10 are");
-    for (  let i=0 ;i<n ;i+=2 )
-    {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
-        console.log(i);
+    let oddnumbers=[]
+    for(i=1;i<n;i+=2){
+        oddnumbers.push(i)
     }
+    return oddnumbers
 }
-DisplayingEvenNumbersLessThanN(10);
+console.log("generate odd numbers less than n", ":",oddnumberslessthann(10))
 
-function DisplayingOddNumbersLessThanN(n)
-{
-    console.log("the odd numbers less than 10 are");
-    for (  let i=1 ;i<n ;i+=2 )
-    {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
-        console.log(i);
+//function to generate n even numbers
+function generatenevennumbers(n){
+    evennumbers=[]
+    for(i=0;evennumbers.length<n;i+=2){
+    evennumbers.push(i)
+}return evennumbers
+}console.log("generate even numbers less than n", ":",generatenevennumbers(10))
+
+//function to generate odd numbers less than n
+function gennoddnumbers(n){
+    oddnumbers=[]
+    for(i=1;oddnumbers.length<n;i+=2){
+        oddnumbers.push(i)
     }
-}
-DisplayingOddNumbersLessThanN(10);
+    return oddnumbers
+}console.log("generate odd numbers less than n", ":",gennoddnumbers(10))
 
-function displayevennumbers(n){
-    console.log("first 10 even numbers are")
-    
-    for (let i=0;evennumbers.length<n;i+=2){
-        
-        console.log(i);
-    }
-return evennumbers ;
-}
-displayevennumbers(10);
-
-
-function displayoddnumbers(n){
-    
-
-    for ( let i=1;oddnumbers.length<n;i+=2){
-        
-        console.log(i)
-    }
-return oddnumbers ;
-}
-displayoddnumbers(10)
